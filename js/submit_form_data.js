5137e1c9c2524a0a2b9bd6444ae8251890179f94
syed <1hk16ec111@hkbk.edu.in>
2023-07-04 14:21:57 +0530

    added a js form that sends email and creates auto leads on pms leads

---
+++ js/submit_form_data.js
@@ -0,0 +1,7 @@
+const form = document.getElementById("contact_form");
+
+const submitButton = document.getElementById("submit-button");
+
+submitButton.addEventListener("click", function () {
+  form.submit();
+});
