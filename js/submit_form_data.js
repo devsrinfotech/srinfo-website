196f59663e0c101dfaee8c73d552573599815a27
syed <1hk16ec111@hkbk.edu.in>
2023-07-14 17:40:13 +0530

    added recaptcha v3 for the form submission

--- js/submit_form_data.js
+++ js/submit_form_data.js
@@ -1,7 +1,41 @@
-const form = document.getElementById("contact_form");
+const form = document
+  .getElementById("contact_form")
+  .addEventListener("submit", async (event) => {
+    event.preventDefault();
 
-const submitButton = document.getElementById("submit-button");
+    const token = await grecaptcha.execute(
+      "6LeLyiEnAAAAAGKq4oVVHOun-sJABia4uWbj4Lcv",
+      { action: "submit" }
+    );
+    console.log(token);
 
-submitButton.addEventListener("click", function () {
-  form.submit();
-});
+    const f_name = document.getElementById("f_name").value;
+    const f_email = document.getElementById("f_email").value;
+    const f_subject = document.getElementById("f_subject").value;
+    const f_phoen = document.getElementById("f_phoen").value;
+    const f_message = document.getElementById("f_message").value;
+
+    console.log(f_name);
+    console.log(f_message);
+    const body = {
+      name: f_name,
+      email: f_email,
+      subject: f_subject,
+      phone: f_phoen,
+      message: f_message,
+      token: token,
+    };
+
+    const params = new URLSearchParams();
+    Object.keys(body).forEach((key) => params.append(key, body[key]));
+
+    formData = params.toString();
+    console.log(formData);
+    await fetch("https://certificate.srinfotech.biz/srinfotechformMail", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: formData,
+    });
+  });
